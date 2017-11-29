292111aa9568badaa1dc79fa90584e3c3882b96d
Manuel <m.a.n.u.e.l@posteo.net>
2017-11-29 21:12:41 +0100

    Local build

--- config/includes.chroot/installer/chroot/build.sh
+++ config/includes.chroot/installer/chroot/build.sh
@@ -82,9 +82,12 @@ curl -sSL https://raw.githubusercontent.com/Linuxbrew/install/master/install | r
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
 rvm install ruby --latest
+rvm use ruby --default
+gem install bundle
 
 # Node Version Manager
 curl -sSL https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+nvm install node
 npm install -g coffeescript
 EOF
 
